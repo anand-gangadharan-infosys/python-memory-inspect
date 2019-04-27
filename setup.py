68024d24d9ad3ca9020e5c4fb0dd10acd13c786a
Anand Gangadharan <anandgangadharan@Anands-MacBook-Air.local>
2019-04-27 13:18:22 +0530

    python tool for obj size and a native module

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-import distutils.log
-
-distutils.log.set_verbosity(-1) # Disable logging in disutils
-distutils.log.set_verbosity(distutils.log.DEBUG) # Set DEBUG level
-
-setup(name='helloworld', version='1.0',  \
-      ext_modules=[Extension('helloworld', ['NativePythonModule.c'])])
